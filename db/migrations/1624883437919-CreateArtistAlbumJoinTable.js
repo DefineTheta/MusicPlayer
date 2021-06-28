d5bbbd29db049702d48f7385dc61bb8d74e7aae8
Parthey Bhatt <partheyb@gmail.com>
2021-06-28 05:52:39 -0700

    Added artist and album join table database migration

---
+++ db/migrations/1624883437919-CreateArtistAlbumJoinTable.js
@@ -0,0 +1,53 @@
+const { Table, TableForeignKey } = require('typeorm');
+
+module.exports = class CreateArtistAlbumJoinTable1624883437919 {
+	async up(queryRunner) {
+		await queryRunner.createTable(
+			new Table({
+				name: 'artist_albums_album',
+				columns: [
+					{
+						name: 'artistId',
+						type: 'integer',
+						isPrimary: true,
+					},
+					{
+						name: 'albumId',
+						type: 'integer',
+						isPrimary: true,
+					},
+				],
+			})
+		);
+
+		await queryRunner.createForeignKey(
+			'artist_albums_album',
+			new TableForeignKey({
+				columnNames: ['artistId'],
+				referencedColumnNames: ['id'],
+				referencedTableName: 'artist',
+				onDelete: 'CASCADE',
+			})
+		);
+
+		await queryRunner.createForeignKey(
+			'artist_albums_album',
+			new TableForeignKey({
+				columnNames: ['albumId'],
+				referencedColumnNames: ['id'],
+				referencedTableName: 'album',
+				onDelete: 'CASCADE',
+			})
+		);
+	}
+
+	async down(queryRunner) {
+		const table = await queryRunner.getTable('artist_albums_album');
+		const foreignKeys = ['artistId', 'albumId'].map((columnName) => {
+			return table.foreignKeys.find((fk) => fk.columnNames.indexOf(columnName) !== -1);
+		});
+
+		await queryRunner.dropForeignKeys('artist_albums_album', foreignKeys);
+		await queryRunner.dropTable('artist_albums_album');
+	}
+};
