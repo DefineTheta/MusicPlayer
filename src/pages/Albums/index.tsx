2df8e680f265b625b1fa48bd1375428e04142145
Theta <partheyb@gmail.com>
2021-06-27 19:27:08 -0700

    Created a base application layout (#1)

---
+++ src/pages/Albums/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AlbumsPage: React.FC = () => {
+	return <div className='w-auto h-auto bg-gray-500'></div>;
+};
+
+export default AlbumsPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				black: {
+					darkest: '#181818',
+					dark: '#121212',
+					DEFAULT: '#000',
+					light: '#282828',
+				},
+			},
+		},
 	},
 	variants: {
 		extend: {},
