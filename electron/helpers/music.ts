b31ae6f09a7e7124cc61642d3887debe64f38e57
Parthey Bhatt <partheyb@gmail.com>
2021-06-28 21:16:21 -0700

    Sorting music metadata by album

--- electron/helpers/music.ts
+++ electron/helpers/music.ts
@@ -1,16 +1,16 @@
 import { parseFile } from 'music-metadata';
 import { getFilesWithExt } from './fs';
 
-// TODO: Have a seperate album artist and song artist
-export const getMusicMetadata = async (
-	filePath: string
-): Promise<{
+export interface IMusicData {
 	title: string | undefined;
 	album: string | undefined;
 	albumArtist: string | undefined;
 	trackPosition: number | null;
 	data: string | undefined;
-}> => {
+}
+
+// TODO: Have a seperate album artist and song artist
+export const getMusicMetadata = async (filePath: string): Promise<IMusicData> => {
 	const metadata = await parseFile(filePath);
 
 	return {
@@ -24,8 +24,19 @@ export const getMusicMetadata = async (
 
 export const parseMusicFiles = async (folderPath: string): Promise<void> => {
 	const musicFilePaths = await getFilesWithExt(folderPath, ['mp3', 'wav', 'flac']);
+	const musicFilesData: { [key: string]: IMusicData[] } = {};
 
 	for (let i = 0; i < musicFilePaths.length; i++) {
-		console.log(await getMusicMetadata(musicFilePaths[i]));
+		const data = await getMusicMetadata(musicFilePaths[i]);
+
+		if (data.album !== undefined) {
+			if (musicFilesData[data.album] !== undefined) {
+				musicFilesData[data.album].push(data);
+			} else {
+				musicFilesData[data.album] = [data];
+			}
+		}
 	}
+
+	console.log(musicFilesData);
 };
