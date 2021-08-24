c4f1445238febe70ea5c642cc71a20d1ae484ae7
Parthey Bhatt <partheyb@gmail.com>
2021-08-23 23:40:42 -0700

    Fetching album data from the database

--- src/pages/Albums/index.tsx
+++ src/pages/Albums/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IpcService } from 'ipc/IpcService';
 
 const AlbumsPage: React.FC = () => {
+	const [albums, setAlbums] = useState();
+	useEffect(() => {
+		const fetchAlbums = async () => {
+			setAlbums(await IpcService.send('get-albums'));
+		};
+
+		fetchAlbums();
+	}, []);
+
 	return <div className='w-auto h-auto bg-gray-500'></div>;
 };
 
