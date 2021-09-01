2035cbd298025305d730daebbe2da37f8ef02a1c
Parthey Bhatt <partheyb@gmail.com>
2021-09-01 16:55:57 -0700

    Added album item card

--- src/pages/Albums/index.tsx
+++ src/pages/Albums/index.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { IpcService } from 'ipc/IpcService';
 import { IAlbum } from 'types/music';
 
+import AlbumItem from './components/AlbumItem';
+
+import './style.scss';
+
 const AlbumsPage: React.FC = () => {
 	const [albums, setAlbums] = useState<IAlbum[]>();
 	useEffect(() => {
@@ -13,15 +17,20 @@ const AlbumsPage: React.FC = () => {
 	}, []);
 
 	if (!albums) {
-		return <div className='w-auto h-auto bg-gray-500'></div>;
+		return <div className='w-auto h-auto p-4'></div>;
 	} else {
 		return (
-			<div className='w-auto h-auto bg-gray-500'>
-				{albums.map((album) => {
-					return (
-						<img src={'local://' + album.coverImagePath} width='128px' key={album.id} />
-					);
-				})}
+			<div className='w-auto h-auto px-6 py-4'>
+				<h1 className='py-2 text-6xl font-semibold'>Albums</h1>
+				<div className='-mr-2 grid albums__container'>
+					{albums.map((album) => (
+						<AlbumItem
+							key={album.id}
+							name={album.name}
+							coverImagePath={album.coverImagePath}
+						/>
+					))}
+				</div>
 			</div>
 		);
 	}
